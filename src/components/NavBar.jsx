cc859344878f7cafca24b3284a3364857ada077a
chriselliott97 <chris.elliott6497@gmail.com>
2022-11-06 19:46:07 -0500

    add NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import Link from 'react-router-dom'
+
+
+const NavBar = () => {
+  return ( 
+    <nav>
+      <Link 
+        to='/'
+      >
+        Your Name
+      </Link>
+
+      <ul>
+        <li>
+          <Link
+            to='/about'
+          >
+            About
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to='/contact'
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;

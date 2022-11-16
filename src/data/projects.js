c03287a2a74578a35fece3fa216e796adaf15a43
chriselliott97 <chris.elliott6497@gmail.com>
2022-11-16 13:49:43 -0500

    addgolf tracker project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,10 @@
 const projects = [
   {
-    title: "Connect Four",
-    description: " Created with JavaScript, HTML and CSS. This is the first project I ever made as a developer and it is meant to highlight my skills with JavaScript. I always loved Connect Four growing up and being able to create the game by myself is a whole new experience. I hope you enjoy the app.",
-    image:"/ConnectFour.png",
-    deploymentLink: "https://chris-elliott-connect-four.netlify.app/"
+    title: "Plan.e",
+    description: "Created with React, HTML, CSS and JavaScript, Plan.e is a react application meant for travevlers of any kind to plan ther trips with various activities submitted from different users and share their experience. Plan.e is designed to make sure one never has to plan a trip alone again. ",
+    image: "/Plan-e.png",
+    repositoryLink: "https://github.com/thepika206/plane-front-end",
+    deploymentLink: "https://wifi-travelers-plane.netlify.app/"
   },
   {
     title: "My Golf Bag",
@@ -13,11 +14,16 @@ const projects = [
     deploymentLink: "https://my-golf-bag.fly.dev/"
   },
   {
-    title: "Plan.e",
-    description: "Created with React, HTML, CSS and JavaScript, Plan.e is a react application meant for travevlers of any kind to plan ther trips with various activities submitted from different users and share their experience. Plan.e is designed to make sure one never has to plan a trip alone again. ",
-    image: "/Plan-e.png",
-    repositoryLink: "https://github.com/thepika206/plane-front-end",
-    deploymentLink: "https://wifi-travelers-plane.netlify.app/"
+    title: "Connect Four",
+    description: " Created with JavaScript, HTML and CSS. This is the first project I ever made as a developer and it is meant to highlight my skills with JavaScript. I always loved Connect Four growing up and being able to create the game by myself is a whole new experience. I hope you enjoy the app.",
+    image:"/ConnectFour.png",
+    deploymentLink: "https://chris-elliott-connect-four.netlify.app/"
+  },
+  {
+    title: "Golf Tracker",
+    description: "Created with Python, Django, HTML, and CSS. This is my first project using Python and is meant to demonstrate the skills that I have learned in python. Gas you can tell, I lve golf and this app allows golfers to track courses that they have played on in the past as well as track the times that they have practiced.",
+    image: "/GolfTracker.png",
+    deploymentLink: "https://golf-course-tracker.herokuapp.com/golfers/3/"
   }
 ]
 

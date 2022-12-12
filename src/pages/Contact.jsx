22c64c9c680a2fcf959ab4be6a1f76e6023abcc9
chriselliott97 <chris.elliott6497@gmail.com>
2022-12-12 14:12:51 -0500

    add mail icon

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,29 @@
+import GITHUB from '../assets/Github-logo.svg'
+import LINKEDIN from '../assets/Linkedin-logo.svg'
+import MAIL from '../assets/Mail.svg'
+
 const Contact = () => {
   return ( 
     <main>
       <h1>Contact me if you would like to work with me!</h1>
-      <h3>Email:
-        <a href="mailto:chris.elliott6497@gmail.com">chris.elliott6497@gmail.com</a>
+      <h3>
+        <a href="mailto:chris.elliott6497@gmail.com">
+          <img style={{width: '60px'}}src={MAIL} alt="linkedin logo"/>
+          chris.elliott6497@gmail.com
+        </a>
       </h3>
       <h3> 
-        <a href="https://www.linkedin.com/in/chris-elliott97/">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/chris-elliott97/">
+          <img style={{width: '60px'}}src={LINKEDIN} alt="linkedin logo"/>
+          LinkedIn
+        </a>
       </h3>
       <h3> 
-        <a href="https://github.com/chriselliott97">GitHub</a>
+        <a href="https://github.com/chriselliott97">
+          <img style={{width: '60px'}}src={GITHUB} alt="github logo" />
+          GitHub
+        </a>
+
       </h3>
 
     </main>
